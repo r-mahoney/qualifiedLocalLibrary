29cf1c43a547c581811a3a052bf6bdd999f56edc
Ryan Mahoney <rsmahoney211@gmail.com>
2023-01-18 11:09:43 -0600

    adding default values to funcions in ./src/ js files

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,27 +1,25 @@
-function findAccountById(accounts, id) {
+function findAccountById(accounts = [], id = "") {
   return accounts.find((account) => (account.id === id));
 }
 
-function sortAccountsByLastName(accounts) {
+function sortAccountsByLastName(accounts = []) {
   return accounts.sort((account1, account2) => (account1.name.last.toLowerCase() > account2.name.last.toLowerCase() ? 1 : -1));
 }
 
-function getTotalNumberOfBorrows(account, books) {
+function getTotalNumberOfBorrows(account = {}, books = []) {
   return books.flatMap(({ borrows }) => borrows.map(({ id }) => (id)))
     .reduce((total, id) => {
       return total + (id === account.id);
     }, 0);
 }
 
-function getBooksPossessedByAccount(account, books, authors) {
+function getBooksPossessedByAccount(account, books = [], authors = []) {
   let borrowed = (books.filter(book => {
     return book.borrows.some((currentBook) => (currentBook.returned === false))
   }))
   let borrowedByAccount = borrowed.filter(book => {
     return book.borrows[0].id === account.id
   })
-
-  
 
   let booksWithAuthor = []
   for (let borrowedBooks of borrowedByAccount) {
